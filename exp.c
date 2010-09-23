fc6b2e24dfc48cd1afc363fb2cfe8de3ea7c37d6
Nuno Martins <nuno.m.g.martins@gmail.com>
2010-09-23 15:24:52 +0100

    some experiments on bits

--- exp.c
+++ exp.c
@@ -3,18 +3,52 @@
 #endif
 #include <linux/module.h>
 #include <linux/sched.h>
+#include <linux/fdtable.h>
+#include <linux/bitops.h>
+#include <linux/types.h>
 #include "filter_module.c"
+
+/*static int count_open_files(struct fdtable *fdt)
+{
+	int size = fdt->max_fds;
+	int i;
+	// Find the last open fd 
+	for (i = size/(8*sizeof(long)); i > 0; ) {
+		if (fdt->open_fds->fds_bits[--i])
+			break;
+	}
+	i = (i+1) * 8 * sizeof(long);
+	return i;
+}
+*/
+
 
 static int __init exp_init(void) {
 	struct task_struct *p;
-	
+	struct files_struct *files;
+	unsigned long i;
+	int j,k;
+	//struct fd_set *fds;
+	struct fdtable *fdt;
+
 	for_each_process(p){
-		printk(KERN_INFO "pid %d and name %s \n",p->pid, p->comm); 
+		if(p->pid == 1674){
+			printk(KERN_INFO "pid %d and name %s \n",p->pid, p->comm);
+			files = p->files;
+			fdt = files->fdt;
+			//fds = fdt->open_fds;
+			i = find_last_bit(fdt->open_fds->fds_bits,64);
+			for(j=0,k=0;j < (int)i;j++)
+			{
+				if(fdt->open_fds->fds_bits[j])
+					k++;
+			}
+			printk(KERN_INFO "has %d files openned\n",k);
+		}
 	}
+
+
 	printk(KERN_INFO "load experiment\n");
-	process_filter();
-	
-	initialize();
     return 0;
 }
 
