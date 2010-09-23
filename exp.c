015147f17dfd505620f231aac8a33e4528029025
Nuno Martins <nuno.m.g.martins@gmail.com>
2010-09-23 21:07:11 +0100

    missing #include <linux/fs.h> fixed

--- exp.c
+++ exp.c
@@ -3,9 +3,13 @@
 #endif
 #include <linux/module.h>
 #include <linux/sched.h>
+#include <linux/types.h>
+#include <linux/fs.h>
+#include <linux/path.h>
+#include <linux/dcache.h>
 #include <linux/fdtable.h>
 #include <linux/bitops.h>
-#include <linux/types.h>
+
 #include "filter_module.c"
 
 /*static int count_open_files(struct fdtable *fdt)
@@ -30,18 +34,37 @@ static int __init exp_init(void) {
 	int j,k;
 	//struct fd_set *fds;
 	struct fdtable *fdt;
+	struct file **fd;
+	struct file *fi;
+	struct path pa;
 
 	for_each_process(p){
 		if(p->pid == 1674){
 			printk(KERN_INFO "pid %d and name %s \n",p->pid, p->comm);
 			files = p->files;
 			fdt = files->fdt;
+			fd = fdt->fd;
 			//fds = fdt->open_fds;
 			i = find_last_bit(fdt->open_fds->fds_bits,64);
-			for(j=0,k=0;j < (int)i;j++)
+			for(j=0,k=0; j < i; j++)
 			{
 				if(fdt->open_fds->fds_bits[j])
+				{
+					struct dentry entry;
+					struct cred *cred;
+
+					fi = *fd;
+					if(fi != NULL)
+					printk(KERN_INFO "ola");
+					pa = fi->f_path;
+//					entry = file->f_dentry;
+//					struct inode *d_inode = file->
+					//printk(KERN_INFO "",);
+					printk(KERN_INFO "fd %d ",j);
+				
 					k++;
+				}
+				fd = fd+1;
 			}
 			printk(KERN_INFO "has %d files openned\n",k);
 		}
