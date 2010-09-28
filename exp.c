9891a305475b0a9d3cf679e627e3913ccd2bd5e6
Nuno Martins <nunogmartins@minilab.me>
2010-09-28 10:15:22 +0100

    knowledge of socket

--- exp.c
+++ exp.c
@@ -9,6 +9,7 @@
 #include <linux/dcache.h>
 #include <linux/fdtable.h>
 #include <linux/bitops.h>
+#include <linux/socket.h>
 
 //#include "filter_module.c"
 struct inode *d_inode = NULL;
@@ -16,6 +17,7 @@ unsigned long howMany = 0L;
 struct file **fd_array;
 struct fdtable *fdt;
 struct file **fd;
+struct socket *s_other;
 
 EXPORT_SYMBOL(fd_array);
 
@@ -32,7 +34,7 @@ int experiment(int pid) {
 	struct files_struct *files;
 //	struct fdtable *fdt;
 //	struct file **fd;
-	
+	int i;
 	for_each_process(p){
 		if(p->pid == pid || pid == -1){
 			printk(KERN_INFO "pid %d and name %s \n",p->pid, p->comm);
@@ -40,6 +42,31 @@ int experiment(int pid) {
 			fdt = files->fdt;
 			fd_array = files->fd_array;
 			fd = fdt->fd;
+			for(i=0; i < 32 ; i++)
+			{
+				struct file *fi = NULL;
+				struct dentry *dentry = NULL;
+				struct inode *d_inode = NULL;
+
+				fi = *(fd_array+i);
+				if(fi == NULL)
+					continue;
+
+				dentry = fi->f_dentry;
+				if(dentry == NULL)
+					continue;
+				d_inode = dentry->d_inode;
+				if(d_inode->i_mode == 49663)
+				{
+					struct socket *s = NULL;
+					printk(KERN_INFO "id %d and is a socket\n",i);
+					s = (struct socket *)fi->private_data;
+					s_other = s;
+				
+				}else 
+					printk(KERN_INFO "id %d and is not a socket\n",i);
+			
+			}
 			
 		}
 		if(p->pid == pid)
