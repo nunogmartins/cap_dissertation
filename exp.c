ca0f4f3e964e788e99030843f70d02523c92ffac
Nuno Martins <nuno.m.g.martins@gmail.com>
2010-10-07 22:55:36 +0100

    added part of kretprobes

--- exp.c
+++ exp.c
@@ -13,6 +13,7 @@
 #include <linux/skbuff.h>
 #include <linux/tcp.h>
 #include <net/inet_sock.h>
+#include <linux/kprobes.h> // -- it's time to get kprobes into the experiment ... 
 
 //#include "filter_module.c"
 struct inode *d_inode = NULL;
@@ -23,12 +24,82 @@ struct file **fd;
 struct socket *s_other;
 struct path f_path;
 
+static char func_name[NAME_MAX] = "do_fork";
+module_param_string(func, func_name, NAME_MAX, S_IRUGO);
+MODULE_PARM_DESC(func, "Function to kretprobe; this module will report the"
+			" function's execution time");
+extern void process_filter(void);
+
+/* per-instance private data */
+struct my_data {
+	ktime_t entry_stamp;
+};
+
+/* Here we use the entry_hanlder to timestamp function entry */
+static int entry_handler(struct kretprobe_instance *ri, struct pt_regs *regs)
+{
+	struct my_data *data;
+
+	if (!current->mm)
+		return 1;	/* Skip kernel threads */
+
+	data = (struct my_data *)ri->data;
+	data->entry_stamp = ktime_get();
+	return 0;
+}
+
+/*
+ * Return-probe handler: Log the return value and duration. Duration may turn
+ * out to be zero consistently, depending upon the granularity of time
+ * accounting on the platform.
+ */
+static int ret_handler(struct kretprobe_instance *ri, struct pt_regs *regs)
+{
+	int retval = regs_return_value(regs);
+	struct my_data *data = (struct my_data *)ri->data;
+	s64 delta;
+	ktime_t now;
+	struct task_struct *task;
+
+	
+	task = (struct task_struct *)ri->task;
+	printk(KERN_INFO "the application is %s with pid %d.\n",task->comm,task->pid);
+	now = ktime_get();
+	delta = ktime_to_ns(ktime_sub(now, data->entry_stamp));
+	printk(KERN_INFO "%s returned %d and took %lld ns to execute\n",
+			func_name, retval, (long long)delta);
+
+	return 0;
+}
+
+
 EXPORT_SYMBOL(fd_array);
+
+static struct kretprobe my_kretprobe = {
+	.handler		= ret_handler,
+	.entry_handler		= entry_handler,
+	.data_size		= sizeof(struct my_data),
+	/* Probe up to 20 instances concurrently. */
+	.maxactive		= 20,
+};
+
 
 static int __init exp_init(void){
 
-d_inode = NULL;
-printk(KERN_INFO "load experiment\n");
+	int ret;
+	d_inode = NULL;
+	printk(KERN_INFO "load experiment\n");
+	
+	my_kretprobe.kp.symbol_name = func_name;
+	ret = register_kretprobe(&my_kretprobe);
+	if (ret < 0) {
+		printk(KERN_INFO "register_kretprobe failed, returned %d\n",
+				ret);
+		return -1;
+	}
+	printk(KERN_INFO "Planted return probe at %s: %p\n",
+			my_kretprobe.kp.symbol_name, my_kretprobe.kp.addr);
+
 return 0;
 
 }
@@ -99,6 +170,7 @@ int experiment(int pid) {
 EXPORT_SYMBOL(experiment);
 
 static void __exit exp_exit(void) {
+	unregister_kretprobe(&my_kretprobe);
 	printk(KERN_INFO "unload experiment\n");
 }
 
