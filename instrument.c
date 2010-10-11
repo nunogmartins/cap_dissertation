3041a3e2425e2b1e2202e5a0e05bf83269d732a7
Nuno Martins <nuno.m.g.martins@gmail.com>
2010-10-11 23:29:51 +0100

    adding instrumentation to sk_attach_filter

---
+++ instrument.c
@@ -0,0 +1,46 @@
+#include <linux/module.h>
+#include <linux/kprobes.h>
+#include <linux/socket.h>
+#include <linux/in.h>
+#include <linux/byteorder/generic.h>
+#include <asm/uaccess.h>
+#include <linux/filter.h>
+
+static long jattach_filter(struct sock_fprog *fprog, struct sock *sk)
+{
+	
+	/* Always end with a call to jprobe_return(). */
+	jprobe_return();
+	return 0;
+}
+
+static struct jprobe my_jprobe = {
+	.entry			= jattach_filter,
+	.kp = {
+		.symbol_name	= "sk_attach_filter",
+	},
+};
+
+static int __init jprobe_init(void)
+{
+	int ret;
+
+	ret = register_jprobe(&my_jprobe);
+	if (ret < 0) {
+		printk(KERN_INFO "register_jprobe failed, returned %d\n", ret);
+		return -1;
+	}
+	printk(KERN_INFO "Planted jprobe at %p, handler addr %p\n",
+	       my_jprobe.kp.addr, my_jprobe.entry);
+	return 0;
+}
+
+static void __exit jprobe_exit(void)
+{
+	unregister_jprobe(&my_jprobe);
+	printk(KERN_INFO "jprobe at %p unregistered\n", my_jprobe.kp.addr);
+}
+
+module_init(jprobe_init)
+module_exit(jprobe_exit)
+MODULE_LICENSE("GPL");
