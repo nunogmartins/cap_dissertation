24da01c71dbede6753d8b00ef286077dde05f6f8
Nuno Martins <nuno.m.g.martins@gmail.com>
2010-10-12 00:47:24 +0100

    added kreprobe to sys_accept

--- instrument.c
+++ instrument.c
@@ -6,6 +6,21 @@
 #include <asm/uaccess.h>
 #include <linux/filter.h>
 
+static struct kretprobe my_kretprobe = {
+	.handler		= ret_handler,
+	.entry_handler		= entry_handler,
+	.data_size		= sizeof(struct my_data),
+	/* Probe up to 20 instances concurrently. */
+	.maxactive		= 20,
+};
+
+static struct jprobe my_jprobe = {
+	.entry			= jattach_filter,
+	.kp = {
+		.symbol_name	= "sk_attach_filter",
+	},
+};
+
 static long jattach_filter(struct sock_fprog *fprog, struct sock *sk)
 {
 	int i = 0;
@@ -21,12 +36,47 @@ static long jattach_filter(struct sock_fprog *fprog, struct sock *sk)
 	return 0;
 }
 
-static struct jprobe my_jprobe = {
-	.entry			= jattach_filter,
-	.kp = {
-		.symbol_name	= "sk_attach_filter",
-	},
+/* per-instance private data */
+struct my_data {
+	ktime_t entry_stamp;
 };
+
+/* Here we use the entry_hanlder to timestamp function entry */
+static int entry_handler(struct kretprobe_instance *ri, struct pt_regs *regs)
+{
+	struct my_data *data;
+
+	if (!current->mm)
+		return 1;	/* Skip kernel threads */
+
+	data = (struct my_data *)ri->data;
+	data->entry_stamp = ktime_get();
+	return 0;
+}
+
+/*
+ * Return-probe handler: Log the return value and duration. Duration may turn
+ * out to be zero consistently, depending upon the granularity of time
+ * accounting on the platform.
+ */
+static int ret_handler(struct kretprobe_instance *ri, struct pt_regs *regs)
+{
+	int retval = regs_return_value(regs);
+	struct my_data *data = (struct my_data *)ri->data;
+	s64 delta;
+	ktime_t now;
+	struct task_struct *task;
+
+	
+	task = (struct task_struct *)ri->task;
+	printk(KERN_INFO "the application is %s with pid %d.\n",task->comm,task->pid);
+	now = ktime_get();
+	delta = ktime_to_ns(ktime_sub(now, data->entry_stamp));
+	printk(KERN_INFO "%s returned %d and took %lld ns to execute\n",
+			func_name, retval, (long long)delta);
+
+	return 0;
+}
 
 static int __init jprobe_init(void)
 {
@@ -39,6 +89,17 @@ static int __init jprobe_init(void)
 	}
 	printk(KERN_INFO "Planted jprobe at %p, handler addr %p\n",
 	       my_jprobe.kp.addr, my_jprobe.entry);
+
+	my_kretprobe.kp.symbol_name = "sys_accept";
+	ret = register_kretprobe(&my_kretprobe);
+	if (ret < 0) {
+		printk(KERN_INFO "register_kretprobe failed, returned %d\n",
+				ret);
+		return -1;
+	}
+	printk(KERN_INFO "Planted return probe at %s: %p\n",
+			my_kretprobe.kp.symbol_name, my_kretprobe.kp.addr);
+
 	return 0;
 }
 
@@ -46,6 +107,8 @@ static void __exit jprobe_exit(void)
 {
 	unregister_jprobe(&my_jprobe);
 	printk(KERN_INFO "jprobe at %p unregistered\n", my_jprobe.kp.addr);
+	unregister_kretprobe(&my_kretprobe);
+	printk(KERN_INFO "kretprobe at %p unregistered\n", my_kretprobe.addr);
 }
 
 module_init(jprobe_init)
