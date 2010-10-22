0d6e07d570d5e89e0a6242bd317a9c3ae3d19289
Nuno Martins <test@minilab.in.di.fct.unl.pt>
2010-10-22 22:42:28 +0100

    get the arguments of the functions that are in the user stack ...

--- instrument_module.c
+++ instrument_module.c
@@ -6,6 +6,10 @@
 #include <asm/uaccess.h>
 #include <linux/filter.h>
 #include <linux/fdtable.h>
+#include <linux/fs.h>
+#include <net/sock.h>
+#include <linux/string.h>
+#include <net/inet_sock.h>
 
 #include "table_port.h"
 
@@ -31,6 +35,15 @@ static int ret_handler(struct kretprobe_instance *ri, struct pt_regs *regs)
 	struct kretprobe *rp = NULL;
 	struct kprobe kp;
 	const char *name = NULL;
+	struct socket *sock = NULL;
+	void *stack = NULL;
+	struct sock *sk;
+	int addr_len = -1;
+	struct sockaddr_in in;
+	struct inet_sock *inet = NULL;
+	int addr;
+	short int my_port = -1;
+	int *p = NULL;
 
 	if (!current->mm)
 		return 1;	/* Skip kernel threads */
@@ -43,6 +56,24 @@ static int ret_handler(struct kretprobe_instance *ri, struct pt_regs *regs)
 	printk(KERN_INFO "%s returned %d application %s\n",
 	name, retval, task->comm);
 
+	//stack = task->stack;
+	stack = regs->bp;
+
+	printk(KERN_INFO "before copy -- size %d bytes", addr_len);
+	memcpy(&addr_len, stack+16, 4);
+	memcpy(&addr,stack+8,4);
+	p = stack+12;
+	memcpy(&in,*p,16);
+
+	//sock = (struct socket *) (stack + 8);
+	//sk = sock->sk;
+	//inet = inet_sk(sk);
+	//in = kmalloc(*in,GFP_KERNEL);
+	//in = (struct sockaddr_in*)(stack + 12);
+	//sk = (struct sock *)(stack - 12);
+	//printk(KERN_INFO "%p and dport = %d",sock, inet->num);	
+printk(KERN_INFO "my port %d and fd = %d",ntohs(in.sin_port),addr);
+	printk(KERN_INFO "after copy -- size %zu bytes", addr_len);
 	return 0;
 }
 
@@ -89,7 +120,7 @@ static int __init instrument_init(void)
     ret = instantiationKRETProbe(kretprobes,"inet_bind",ret_handler,entry_handler);
 	if(ret < 0)
 		return -1;
-
+/*
 
     ret = instantiationKRETProbe(kretprobes+1,"tcp_fin",ret_handler,entry_handler);
 	if(ret < 0)
@@ -99,6 +130,7 @@ static int __init instrument_init(void)
     ret = instantiationKRETProbe(kretprobes+2,"tcp_close",ret_handler,entry_handler);
 	if(ret < 0)
 		return -1;
+*/
     //register all probes
 	return 0;
 }
@@ -108,13 +140,13 @@ static void __exit instrument_exit(void)
     //unregister all probes ...
     unregister_kretprobe(kretprobes);
 	printk(KERN_INFO "kretprobe at %p unregistered\n", kretprobes->kp.addr);
-
+/*
     unregister_kretprobe(kretprobes+1);
 	printk(KERN_INFO "kretprobe at %p unregistered\n", (kretprobes+1)->kp.addr);
     
 	unregister_kretprobe(kretprobes+2);
 	printk(KERN_INFO "kretprobe at %p unregistered\n", (kretprobes+2)->kp.addr);
-    
+*/    
 	if(kretprobes)
         kfree(kretprobes);
     if(jprobes)
