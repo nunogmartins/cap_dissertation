17786429de1a03bf82d945b16f0f2521625b2067
Nuno Martins <nuno.m.g.martins@gmail.com>
2010-11-22 17:19:26 +0000

    added some new functions to deal with inodes and files

--- tools.c
+++ tools.c
@@ -16,6 +16,35 @@
 
 #include "pcap_monitoring.h"
 
+struct inode getInodeFromFd(unsigned int fd)
+{
+	struct inode *d_inode = NULL;
+	struct file *f = NULL;
+
+	f = fget(fd);
+
+	if(f!=NULL)
+	{
+		struct dentry *dentry = NULL;
+
+		fput(f);
+		dentry = f->f_dentry;
+		if(dentry!=NULL)
+		{
+			d_inode = dentry->d_inode;
+		}
+	}
+
+	return d_inode;
+}
+
+
+struct short getPortFromInode()
+{
+	struct
+	return ;
+}
+
 /*
  * unsigned int fd
  * int direction
