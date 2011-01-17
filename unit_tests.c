9a65cac12c539d400c6e431f4abc65127be1171a
Nuno Martins <nuno.m.g.martins@gmail.com>
2011-01-17 17:14:46 +0000

    trying to test lists using unit tests

--- unit_tests.c
+++ unit_tests.c
@@ -5,34 +5,31 @@
 
 #include <linux/slab.h>
 
-#include "portsDB.h"
+#include "table_port.h"
 
 #define INITIAL_PORT 2010
 #define FINAL_PORT 2050
 
 #define MAX_DATA 5
 
-extern struct rb_root db;
-
-static struct portInfo *ports;
+static struct packetInfo *ports;
 
 int populate(void)
 {
 	int i=0;
 	int iteration = 0;
 
-	ports = kmalloc(MAX_DATA*sizeof(struct portInfo),GFP_KERNEL);
+	ports = kmalloc(MAX_DATA*sizeof(*ports),GFP_KERNEL);
 
-	for(i=INITIAL_PORT;i < FINAL_PORT; i+=10)
+	for(i=INITIAL_PORT;i < FINAL_PORT; i+=10,iteration++)
 	{
 		//create port to use in my_insert
-		struct portInfo *sentinel = (ports)+iteration;
+		struct packetInfo *sentinel = (ports)+iteration;
 		sentinel->port = (u16)i;
 		sentinel->address = 0x7f000001;
 		sentinel->protocol = 0x06;
-		//my_insert();
-		my_insert(&db, sentinel);
-		iteration++;
+		insertPort(sentinel);
+		printTree();
 	} 
 	
 	return 0;	
@@ -41,15 +38,17 @@ int populate(void)
 int depopulate(void)
 {
 	int i = INITIAL_PORT;
-	printAll(&db);
+	int iteration = 0;
+	printTree();
 	//do for all ports my_erase
-	for( ; i < FINAL_PORT ; i+=10)
+	for(i=INITIAL_PORT;i < FINAL_PORT; i+=10,iteration++)
 	{
-		my_erase(&db,(u16)i);
+		deletePort((ports+iteration));
+		printTree();
 	}
+
 	kfree(ports);
 	ports = NULL;
-
 	return 0;
 }
 
