3d9a38b8db92f8120ed6f46f13310bf249e357a8
Nuno Martins <nuno.m.g.martins@gmail.com>
2011-01-18 00:03:21 +0000

    multiple addresses

--- unit_tests.c
+++ unit_tests.c
@@ -16,21 +16,24 @@ static struct packetInfo *ports;
 
 int populate(void)
 {
-	int i=0;
+	int i=0, j=0;
 	int iteration = 0;
 
 	ports = kmalloc(MAX_DATA*sizeof(*ports),GFP_KERNEL);
 
-	for(i=INITIAL_PORT;i < FINAL_PORT; i+=10,iteration++)
-	{
-		struct packetInfo *sentinel = (ports)+iteration;
-		sentinel->port = (u16)i;
-		sentinel->address = 0x7f000001;
-		sentinel->protocol = 0x06;
-		insertPort(sentinel);
-		printTree();
-	} 
-	
+	for(j=0;j < 10; j++){
+		for(i=INITIAL_PORT;i < FINAL_PORT; i+=10,iteration++)
+		{
+			struct packetInfo *sentinel = (ports)+iteration;
+			sentinel->port = (u16)i;
+			sentinel->address = 0x7f000001+(j);
+			sentinel->protocol = 0x06;
+			insertPort(sentinel);
+		}
+
+
+	}
+	printTree();
 	return 0;	
 }
 
