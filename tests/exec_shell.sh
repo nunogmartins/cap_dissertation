9d4a89f9b299569f8f3eeb9d3b10074d4deb5dd0
nunolabs <test@ubuntu.(none)>
2011-05-23 16:17:09 +0100

    using script to get the time in times file so that an extraction tool can get the values to create charts

--- tests/exec_shell.sh
+++ tests/exec_shell.sh
@@ -4,7 +4,7 @@ for i in {0..9}
 do
 	beforeRX=$(ifconfig eth0 | grep "RX packets" | awk '{print($2) }' | awk 'BEGIN{FS=":"}{$2=$2}1' |awk '{print($2)}')
 	beforeTX=$(ifconfig eth0 | grep "TX packets" | awk '{print($2) }' | awk 'BEGIN{FS=":"}{$2=$2}1' |awk '{print($2)}')
-	time ./manager -p -t -m -c config.cfg
+	script -a times.$i.log -c "time ./manager -p -t -m -c config.cfg * 2> output.$i.log"
 	#sleep 10
 	afterRX=$(ifconfig eth0 | grep "RX packets" | awk '{print($2) }' | awk 'BEGIN{FS=":"}{$2=$2}1' |awk '{print($2)}')
 	afterTX=$(ifconfig eth0 | grep "TX packets" | awk '{print($2) }' | awk 'BEGIN{FS=":"}{$2=$2}1' |awk '{print($2)}')
